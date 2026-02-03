4e7a5e7fd206d8543303f3a1754588648b31d990
Ramón García Luengo <ramon.garcia966@comunidadunir.net>
2026-02-03 17:30:24 +0100

    refactor: se ha movido la clase que se encarga de arrachar la aplicación para que escanee correctamente

--- src/main/java/es/unir/dwfs/MsBooksCatalogueApplication.java
+++ src/main/java/es/unir/dwfs/MsBooksCatalogueApplication.java
@@ -1,4 +1,4 @@
-package es.unir.dwfs.ms_books_catalogue;
+package es.unir.dwfs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
